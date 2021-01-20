3b3a21dfdddf2df23d1b7c67239fc83466f82d28
adelakim <diddnjs02@likelion.org>
2021-01-20 12:49:33 +0900

    correct: mockTest

--- level1/mockTest/main.js
+++ level1/mockTest/main.js
@@ -1,11 +1,22 @@
-let people = [2, 2, 2];
-let max = Math.max(...people);
-let temp = people.map((e, i) => {
-  if (e >= max) return i + 1;
-  else continue;
-});
-// let temp = [...people.filter((e) => e >= max).keys()].map((e) => e + 1);
-// let temp = [...people.keys()];
-console.log(temp);
+function solution(answers) {
+  var answer = [];
+  const people = [0, 0, 0];
+  const p1 = [1, 2, 3, 4, 5];
+  const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+    if (answer === p1[i % p1.length]) people[0]++;
+    if (answer === p2[i % p2.length]) people[1]++;
+    if (answer === p3[i % p3.length]) people[2]++;
+  }
+  return sort(people, answer);
+}
 
-const transpose = (matrix) => matrix.reduce((result, row) => row.map((_, i) => [...(result[i] || []), row[i]]), []);
+function sort(people, answer) {
+  const max = Math.max(...people);
+  for (let i = 0; i < 3; i++) {
+    if (people[i] >= max) answer.push(i + 1);
+  }
+  return answer;
+}
