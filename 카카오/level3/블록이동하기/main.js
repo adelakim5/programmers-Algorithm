1968043afe1625764982686392c4f4b78f18dea5
adelakim <diddnjs02@likelion.org>
2021-01-24 18:32:09 +0900

    correct: 블록 이동하기

--- 카카오/level3/블록이동하기/main.js
+++ 카카오/level3/블록이동하기/main.js
@@ -1,123 +1,125 @@
-// let board = [
-//   [0, 0, 0, 1, 1],
-//   [0, 0, 0, 1, 0],
-//   [0, 1, 0, 1, 1],
-//   [1, 1, 0, 0, 1],
-//   [0, 0, 0, 0, 0],
-// ];
-
-let board = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-];
-console.log(solution(board));
-
-function pushToQ(visited, key, value, q, dir, currTime) {
-  const [kx, ky] = key;
-  const [vx, vy] = value;
-  visited[`${kx} ${ky}`].push(`${vx} ${vy}`);
-  q.push({ d: dir, x1: kx, y1: ky, x2: vx, y2: vy, time: currTime + 1, visited });
-  return q;
-}
-
-function checkVisited(visited, key, value) {
-  const [kx, ky] = key;
-  const [vx, vy] = value;
-  if (!visited[`${kx} ${ky}`]) return true;
-  if (!visited[`${kx} ${ky}`].valueOf().some((val) => val === `${vx} ${vy}`)) return true;
-  return false;
-}
-
 function solution(board) {
-  const n = board.length;
-  let newBoard = setBoard(board, n);
-  let robot = { d: 2, x1: 1, y1: 1, x2: 1, y2: 2, time: 0, visited: {} };
-  let q = [];
-  let min = Infinity;
-  robot.visited[`${robot.x1} ${robot.y1}`] = [`${robot.x2} ${robot.y2}`];
-  q.push(robot);
-  // console.log(robot.visited[`${robot.x1} ${robot.y1}`].valueOf(`${robot.x2} ${robot.y2}`));
+  let answer = 0;
+  const newBoard = setNewBoard(board);
+  const robot = { d: 2, x: 1, y: 1, time: 0 };
+  answer = bfs(robot, board.length, newBoard);
+  return answer;
+}
 
-  while (q.length) {
-    const { d, x1, y1, x2, y2, time, visited } = q.shift();
-    // console.log(visited[`${x1} ${y1}`] ? true : false);
-    // console.log(d, x1, y1, x2, y2, time);
-    // if (x1 === 5 && y1 === 3 && x2 === 5 && y2 === 4) {
-    //   console.log(`hello world`);
-    //   console.log(visited);
-    // }
-
-    if (x2 === n && y2 === n) {
-      min = Math.min(time, min);
-      continue;
+function setVisit(arr, n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      arr[i][j] = { d1: false, d2: false };
     }
+  }
+  return arr;
+}
+
+function bfs(robot, n, newBoard) {
+  let min = Infinity;
+  let q = [];
+  let visit = Array.from(Array(n + 2), () => Array(n + 2));
+  visit = setVisit(visit, n + 2);
+  q.push(robot);
+  visit[robot.x][robot.y].d2 = true;
+  while (q.length) {
+    const { d, x, y, time } = q.shift();
     if (d === 2) {
-      if (!newBoard[x1 + 1][y1] && !newBoard[x2 + 1][y2]) {
-        if (checkVisited(visited, [x1, y1], [x1 + 1, y1])) {
-          if (!visited[`${x1} ${y1}`]) visited[`${x1} ${y1}`] = [];
-          q = pushToQ(visited, [x1, y1], [x1 + 1, y1], q, 1, time);
+      // 로봇이 가로
+      if ((x === n && y === n) || (x === n && y + 1 === n)) {
+        min = Math.min(min, time);
+        continue;
+      }
+      if (checkUp(x, y, newBoard, d)) {
+        if (!visit[x - 1][y].d2) {
+          // 상
+          q.push({ d, x: x - 1, y, time: time + 1 });
+          visit[x - 1][y].d2 = true;
         }
-        if (checkVisited(visited, [x2, y2], [x2 + 1, y2])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2 + 1, y2], q, 1, time);
+        if (!visit[x - 1][y].d1) {
+          // 첫번째 칸을 기준으로 반시계 회전
+          q.push({ d: 1, x: x - 1, y, time: time + 1 });
+          visit[x - 1][y].d1 = true;
+        }
+        if (!visit[x - 1][y + 1].d1) {
+          // 두번째 칸을 기준으로 시계 회전
+          q.push({ d: 1, x: x - 1, y: y + 1, time: time + 1 });
+          visit[x - 1][y + 1].d1 = true;
         }
       }
-      if (!newBoard[x1 - 1][y1] && !newBoard[x2 - 1][y2]) {
-        if (checkVisited(visited, [x1, y1], [x1 - 1, y1])) {
-          if (!visited[`${x1} ${y1}`]) visited[`${x1} ${y1}`] = [];
-          q = pushToQ(visited, [x1, y1], [x1 - 1, y1], q, 1, time);
+      if (checkBottom(x, y, newBoard, d)) {
+        if (!visit[x + 1][y].d2) {
+          // 하
+          q.push({ d, x: x + 1, y, time: time + 1 });
+          visit[x + 1][y].d2 = true;
         }
-        if (checkVisited(visited, [x2, y2], [x2 - 1, y2])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2 - 1, y2], q, 1, time);
+        if (!visit[x][y].d1) {
+          // 첫번째 칸을 기준으로 시계 회전
+          q.push({ d: 1, x, y, time: time + 1 });
+          visit[x][y].d1 = true;
+        }
+        if (!visit[x][y + 1].d1) {
+          // 두번째 칸을 기준으로 반시계 회전
+          q.push({ d: 1, x, y: y + 1, time: time + 1 });
+          visit[x][y + 1].d1 = true;
         }
       }
-      if (!newBoard[x1][y1 - 1]) {
-        if (checkVisited(visited, [x1, y1 - 1], [x1, y1])) {
-          if (!visited[`${x1} ${y1 - 1}`]) visited[`${x1} ${y1 - 1}`] = [];
-          q = pushToQ(visited, [x1, y1 - 1], [x1, y1], q, 2, time);
-        }
+      if (checkLeft(x, y, newBoard, d) && !visit[x][y - 1].d2) {
+        // 좌
+        q.push({ d, x, y: y - 1, time: time + 1 });
+        visit[x][y - 1].d2 = true;
       }
-      if (!newBoard[x2][y2 + 1]) {
-        if (checkVisited(visited, [x2, y2], [x2, y2 + 1])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2, y2 + 1], q, 2, time);
-        }
+      if (checkRight(x, y, newBoard, d) && !visit[x][y + 1].d2) {
+        // 우
+
+        q.push({ d, x, y: y + 1, time: time + 1 });
+        visit[x][y + 1].d2 = true;
       }
     } else {
-      if (!newBoard[x1][y1 + 1] && !newBoard[x2][y2 + 1]) {
-        if (checkVisited(visited, [x1, y1], [x1, y1 + 1])) {
-          if (!visited[`${x1} ${y1}`]) visited[`${x1} ${y1}`] = [];
-          q = pushToQ(visited, [x1, y1], [x1, y1 + 1], q, 2, time);
+      // 로봇이 세로
+      if ((x === n && y === n) || (x + 1 === n && y === n)) {
+        min = Math.min(min, time);
+        continue;
+      }
+      if (checkUp(x, y, newBoard, d) && !visit[x - 1][y].d1) {
+        q.push({ d, x: x - 1, y, time: time + 1 });
+        visit[x - 1][y].d1 = true;
+      }
+      if (checkBottom(x, y, newBoard, d) && !visit[x + 1][y].d1) {
+        q.push({ d, x: x + 1, y, time: time + 1 });
+        visit[x + 1][y].d1 = true;
+      }
+      if (checkLeft(x, y, newBoard, d)) {
+        if (!visit[x][y - 1].d1) {
+          q.push({ d, x, y: y - 1, time: time + 1 });
+          visit[x][y - 1].d1 = true;
         }
-        if (checkVisited(visited, [x2, y2], [x2, y2 + 1])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2, y2 + 1], q, 2, time);
+        if (!visit[x][y - 1].d2) {
+          // 첫번째 칸을 기준으로 시계 회전
+          q.push({ d: 2, x, y: y - 1, time: time + 1 });
+          visit[x][y - 1].d2 = true;
+        }
+        if (!visit[x + 1][y - 1].d2) {
+          // 두번째 칸을 기준으로 반시계 회전
+          q.push({ d: 2, x: x + 1, y: y - 1, time: time + 1 });
+          visit[x + 1][y - 1].d2 = true;
         }
       }
-      if (!newBoard[x1][y1 - 1] && !newBoard[x2][y2 - 1]) {
-        if (checkVisited(visited, [x1, y1], [x1, y1 - 1])) {
-          if (!visited[`${x1} ${y1}`]) visited[`${x1} ${y1}`] = [];
-          q = pushToQ(visited, [x1, y1], [x1, y1 - 1], q, 2, time);
+      if (checkRight(x, y, newBoard, d)) {
+        if (!visit[x][y + 1].d1) {
+          // 우
+          q.push({ d, x, y: y + 1, time: time + 1 });
+          visit[x][y + 1].d1 = true;
         }
-        if (checkVisited(visited, [x2, y2], [x2, y2 - 1])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2, y2 - 1], q, 2, time);
+        if (!visit[x][y].d2) {
+          // 첫째칸을 기준으로 반시계 회전
+          q.push({ d: 2, x, y, time: time + 1 });
+          visit[x][y].d2 = true;
         }
-      }
-      if (!newBoard[x1 - 1][y1]) {
-        if (checkVisited(visited, [x1 - 1, y1], [x1, y1])) {
-          if (!visited[`${x1 - 1} ${y1}`]) visited[`${x1 - 1} ${y1}`] = [];
-          q = pushToQ(visited, [x1 - 1, y1], [x1, y1], q, 1, time);
-        }
-      }
-      if (!newBoard[x2 + 1][y2]) {
-        if (checkVisited(visited, [x2, y2], [x2 + 1, y2])) {
-          if (!visited[`${x2} ${y2}`]) visited[`${x2} ${y2}`] = [];
-          q = pushToQ(visited, [x2, y2], [x2 + 1, y2], q, 1, time);
+        if (!visit[x + 1][y].d2) {
+          // 두번째 칸을 기준으로 시계 회전
+          q.push({ d: 2, x: x + 1, y, time: time + 1 });
+          visit[x + 1][y].d2 = true;
         }
       }
     }
@@ -125,7 +127,32 @@ function solution(board) {
   return min;
 }
 
-function setBoard(board, n) {
+function checkUp(x, y, board, d) {
+  if (d === 2 && board[x - 1][y] === 0 && board[x - 1][y + 1] === 0) return true;
+  if (d === 1 && board[x - 1][y] === 0) return true;
+  return false;
+}
+
+function checkBottom(x, y, board, d) {
+  if (d === 2 && board[x + 1][y] === 0 && board[x + 1][y + 1] === 0) return true;
+  if (d === 1 && board[x + 2][y] === 0) return true;
+  return false;
+}
+
+function checkLeft(x, y, board, d) {
+  if (d === 2 && board[x][y - 1] === 0) return true;
+  if (d === 1 && board[x][y - 1] === 0 && board[x + 1][y - 1] === 0) return true;
+  return false;
+}
+
+function checkRight(x, y, board, d) {
+  if (d === 2 && board[x][y + 2] === 0) return true;
+  if (d === 1 && board[x][y + 1] === 0 && board[x + 1][y + 1] === 0) return true;
+  return false;
+}
+
+function setNewBoard(board) {
+  const n = board.length;
   let newBoard = Array.from(Array(n + 2), () => Array(n + 2).fill(1));
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
