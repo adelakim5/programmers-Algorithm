65193202a22019ecdd11750fca8fb149956c301d
adelakim <diddnjs02@likelion.org>
2021-04-20 19:21:32 +0900

    correct: n으로표현

---
+++ 다시풀기/고득점kit/동적계획법/n으로표현-2.js
@@ -0,0 +1,20 @@
+function solution(N, number) {
+  const d = Array.from(Array(9), () => new Set());
+  for (let i = 1; i < 9; i++) {
+    d[i].add(Number(N.toString().repeat(i)));
+    for (let j = 1; j < i; j++) {
+      for (let a1 of d[j]) {
+        for (let a2 of d[i - j]) {
+          d[i].add(a1 + a2);
+          d[i].add(a1 - a2);
+          d[i].add(a1 * a2);
+          d[i].add(Math.floor(a1 / a2));
+        }
+      }
+      if (d[i].has(number)) return i;
+    }
+  }
+  return -1;
+}
+
+console.log(solution(5, 12));
